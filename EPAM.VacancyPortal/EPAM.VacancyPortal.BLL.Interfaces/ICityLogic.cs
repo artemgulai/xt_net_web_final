a91dd41b750d1bdb6000864e0b2ef748711ee008
Gulai Artem <gulai.artem@gmail.com>
2020-02-24 12:07:46 +0400

    Add Summary

--- EPAM.VacancyPortal/EPAM.VacancyPortal.BLL.Interfaces/ICityLogic.cs
+++ EPAM.VacancyPortal/EPAM.VacancyPortal.BLL.Interfaces/ICityLogic.cs
@@ -9,10 +9,38 @@ namespace EPAM.VacancyPortal.BLL.Interfaces
 {
     public interface ICityLogic
     {
+        /// <summary>
+        /// Invokes DAL's Insert method.
+        /// </summary>
+        /// <param name="city">City to be inserted into DB.</param>
+        /// <returns>City with Id != 0 if it is inserted, otherwise Id = 0.</returns>
         City Insert(City city);
+
+        /// <summary>
+        /// Invokes DAL's GetAll method.
+        /// </summary>
+        /// <returns>IEnumerable of Cities if a query is successfull, 
+        /// otherwise empty List of Cities.</returns>
         IEnumerable<City> GetAll();
+
+        /// <summary>
+        /// Invokes DAL's GetIdByName method.
+        /// </summary>
+        /// <param name="cityName">A Name of a City for which an Id is got.</param>
+        /// <returns>Id if such City exists, otherwise 0.</returns>
         int GetIdByName(string cityName);
+
+        /// <summary>
+        /// Invokes DAL's DeleteById method.
+        /// </summary>
+        /// <param name="id">Id of a City to be deleted.</param>
+        /// <returns>Number of deleted rows.</returns>
         int DeleteById(int id);
+
+        /// <summary>
+        /// Invokes DAL's DeleteAll method.
+        /// </summary>
+        /// <returns>True if a query is successfull, otherwise false.</returns>
         bool DeleteAll();
     }
 }
