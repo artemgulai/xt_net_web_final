699f04984da3796ec61dc162fc18aeca030ea1f5
Gulai Artem <gulai.artem@gmail.com>
2020-02-24 11:40:18 +0400

    Add stored procedures for Cities table

--- SQL/DB-StoredProcedures.sql
+++ SQL/DB-StoredProcedures.sql
@@ -15,3 +15,19 @@ AS
 	SELECT ID FROM Cities
 	WHERE Name = @Name
 GO
+
+CREATE PROCEDURE [dbo].[sp_DeleteCityById]
+	@Id INT
+AS
+	DELETE FROM Cities WHERE Id = @Id
+GO
+
+CREATE PROCEDURE [dbo].[sp_DeleteAllCities]
+AS
+	DELETE FROM Cities
+GO
+
+CREATE PROCEDURE [dbo].[sp_GetAllCities]
+AS
+	SELECT * FROM Cities
+GO
